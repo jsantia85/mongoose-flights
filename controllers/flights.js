f4142a3aea68dd56929fe031eb5b72bb0aa9c76b
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-19 15:25:53 -0400

    adds create functionality and all flights ui

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,9 +1,27 @@
+import { Flight } from "../models/flight.js"
+
 function newFlight(req, res) {
   res.render("flights/new", {
     title: "Add Flight",
   })
 }
 
+function create(req, res) {
+  Flight.create(req.body)
+  .then(flight => {
+    res.redirect(`/flights/new`)
+  })
+  .catch(err => {
+    res.redirect('/flights/new')
+  })
+}
+
+function index(req, res) {
+  console.log('this works')
+}
+
 export {
   newFlight as new,
+  create,
+  index,
 }
--- routes/flights.js
+++ routes/flights.js
@@ -2,8 +2,14 @@ import { Router } from 'express'
 const router = Router()
 import * as flightsCtrl from '../controllers/flights.js'
 
-// GET /flights/new
+//GET localhost:3000/flights
+router.get('/', flightsCtrl.index)
+
+//GET localhost:3000/flights/new
 router.get('/new', flightsCtrl.new)
+
+//POST localhost:3000/flights
+router.post('/', flightsCtrl.create)
 
 export {
 	router
