5b58e034c44205f1542f31e621a99b1c05cf8bef
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-21 17:13:37 -0400

    creates ticket form and route for /:id/tickets

--- routes/flights.js
+++ routes/flights.js
@@ -8,14 +8,17 @@ router.get('/', flightsCtrl.index)
 //GET localhost:3000/flights/new
 router.get('/new', flightsCtrl.new)
 
-// GET -- localhost:3000/flights/:id
+//GET localhost:3000/flights/:id
 router.get('/:id', flightsCtrl.show)
 
-// localhost:3000/movies/:id/edit
+//GET localhost:3000/movies/:id/edit
 router.get("/:id/edit", flightsCtrl.edit)
 
 //POST localhost:3000/flights
 router.post('/', flightsCtrl.create)
+
+//POST localhost:3000/flights/:id/tickets
+router.post('/:id/tickets', flightsCtrl.createTicket)
 
 // localhost:3000/flights/:id
 router.delete("/:id", flightsCtrl.delete)
