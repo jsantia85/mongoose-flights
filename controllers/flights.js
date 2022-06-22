0f256ce768acf4463b3cc203240e7179ecc7ea6e
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-22 17:55:11 -0400

    refactors show controller in flights.js

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,4 +1,5 @@
 import { Flight } from "../models/flight.js"
+import { Meal } from '../models/meal.js'
 
 function newFlight(req, res) {
   res.render("flights/new", {
@@ -34,10 +35,14 @@ function show(req, res) {
   Flight.findById(req.params.id)
   .populate('meals')
   .then(flight => {
-    res.render('flights/show', { 
-      title: 'Flight Detail', 
-      flight: flight,
-    })    
+    Meal.find({_id: {$nin: flight.meals}})
+    .then(meals => {
+      res.render('flights/show', {
+        title: 'Flight Detail', 
+        flight: flight,
+        meals: meals,
+      })
+    })
   })
   .catch(err => {
     console.log(err)
