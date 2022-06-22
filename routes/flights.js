fdc97f98021f6dc3fd9e240924bd99234be4d533
JASantia85 <JuanASantiago1058@gmail.com>
2022-06-22 18:07:47 -0400

    adds post route for /:id/meals

--- routes/flights.js
+++ routes/flights.js
@@ -20,6 +20,9 @@ router.post('/', flightsCtrl.create)
 //POST localhost:3000/flights/:id/tickets
 router.post('/:id/tickets', flightsCtrl.createTicket)
 
+//POST localhost:3000/flights/:id/meals
+router.post('/:id/meals', flightsCtrl.addToMeals)
+
 // localhost:3000/flights/:id
 router.delete("/:id", flightsCtrl.delete)
 
